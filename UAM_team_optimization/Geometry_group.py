40021178f6c3ed2ebf5eb094869888075d15b858
Marius Ruh <mruh@ucsd.edu>
2020-05-26 19:51:48 -0700

    From Marius #5

--- UAM_team_optimization/Geometry_group.py
+++ UAM_team_optimization/Geometry_group.py
@@ -1,6 +1,6 @@
 from openmdao.api import Group, IndepVarComp
 
-from lsdo_utils.api import PowerCombinationComp, LinearCombinationComp
+# from lsdo_utils.api import PowerCombinationComp, LinearCombinationComp
 from UAM_team_optimization.components.Geometry.geometry_comp import GeometryComp
 from UAM_team_optimization.components.Geometry.test_geometry_comp import TestGeometryComp 
 
